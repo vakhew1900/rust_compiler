b196cec96bf87187a9f0c20b7928bc7491a640e1
Чупинин Антон <chupinin@intervolga.ru>
2023-09-21 00:07:57 +0300

    [lexer] Первые шаги в парсинге.

--- compiler/main.c
+++ compiler/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "lex.yy.c"
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    const char* filename = "E:\\4 course\\tl\\rust_compiler\\compiler\\test.txt";
+    FILE *file = fopen(filename, "r");
+    yyin = file;
+
+    while (yylex() != 0);
+    fclose(file);
+
     return 0;
 }
+
