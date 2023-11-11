20f957b3f6f03ba5964c56f0bced6a4679fdb2dd
Чупинин Антон <chupinin@intervolga.ru>
2023-11-12 01:14:00 +0300

    [test] добавлены тесты

---
+++ compiler/tests/simple_else_if.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+    let a = 10;
+    
+    if (a > 10)
+    {
+        println(">10");
+    }
+    else if (a < 10 && a > 0)
+    {
+        println("between 10 and 0");
+    }
+    else
+    {
+        println("< 0");
+    };
+}
