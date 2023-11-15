77708a64c8f76e62be2da2459ea16b26350dc3ed
gogenzollern <arseny.lomakin@gmail.com>
2023-11-15 22:23:57 +0300

    правка теста

--- compiler/tests/conflict_test3.rs
+++ compiler/tests/conflict_test3.rs
@@ -4,7 +4,7 @@ fn main() {
     if number < 10 {
         println("Число меньше 10.");
     } else {
-        if (10..=20).contains(&number) {
+        if (10..20).contains(&number) {
             println("Число находится в диапазоне от 10 до 20 включительно.");
         } else {
             println("Число больше 20.");
