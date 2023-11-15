33890229dc21083593d64312c171b9a0d352fc42
gogenzollern <arseny.lomakin@gmail.com>
2023-11-15 21:27:14 +0300

    simple_if_else без ; НЕ ПРОЙДЕН

---
+++ compiler/tests/simple_if_else_without_semicolon.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let a = 10;
+
+    if (a > 10)
+    {
+        println(">10");
+    }
+    else if (a < 10 && a > 0)
+    {
+        println("between 10 and 0");
+    }
+    else
+    {
+        println("< 0");
+    }
+}
