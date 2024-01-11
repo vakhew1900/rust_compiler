22b0d278c147c4afbeb93ec4fdb9afd54d72c427
Чупинин Антон <chupinin@intervolga.ru>
2024-01-11 23:22:22 +0300

    [codegeneration] добавлен файл генератор класса

---
+++ compiler/code_generation/code_generator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by T0mmy on 11.01.2024.
+//
+
+#include "code_generator.h"
---
+++ compiler/code_generation/code_generator.h
@@ -0,0 +1,14 @@
+//
+// Created by T0mmy on 11.01.2024.
+//
+
+#ifndef COMPILER_CODE_GENERATOR_H
+#define COMPILER_CODE_GENERATOR_H
+
+
+class CodeGenerator {
+
+};
+
+
+#endif //COMPILER_CODE_GENERATOR_H
