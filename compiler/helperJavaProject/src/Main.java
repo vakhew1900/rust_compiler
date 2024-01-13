af29830c2ba657496fb175adc47d854000211ac9
gogenzollern <arseny.lomakin@gmail.com>
2024-01-13 14:18:52 +0300

    Добавлен help project с модулями

---
+++ compiler/helperJavaProject/src/Main.java
@@ -0,0 +1,13 @@
+import dir.*;
+
+public class Main {
+    public static void main(String[] args) {
+        One o1 = new One();
+        Two t2 = new Two();
+        Three t3 = new Three();
+
+
+
+        System.out.println(j + " " + k);
+    }
+}
