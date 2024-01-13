00374814bace05523b75253ef57b4674726a7e76
gogenzollern <arseny.lomakin@gmail.com>
2024-01-13 14:19:53 +0300

    [helper] Разобран for с continue и break на байты

--- compiler/helperJavaProject/src/Main.java
+++ compiler/helperJavaProject/src/Main.java
@@ -6,7 +6,21 @@ public class Main {
         Two t2 = new Two();
         Three t3 = new Three();
 
+        int j = 0;
+        for (int i = 0; i < 4; i++){
+            if (i == 2) {
+                break;
+            }
+            j += i;
+        }
 
+        int k = 0;
+        for (int i = 0; i < 4; i++){
+            if (i % 2 == 0) {
+                continue;
+            }
+            k += i;
+        }
 
         System.out.println(j + " " + k);
     }
