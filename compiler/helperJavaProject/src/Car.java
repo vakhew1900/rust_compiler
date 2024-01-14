4c2fb29afd81fd1e88d9cf15f2a148b5df921e81
gogenzollern <arseny.lomakin@gmail.com>
2024-01-14 16:11:31 +0300

    [helper] Проверка на статик филды

---
+++ compiler/helperJavaProject/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    static int speed = 100;
+    static int probeg = 100000;
+    public static void ride(){
+        System.out.println(speed);
+        System.out.println(probeg);
+    }
+}
