367d24e07f9cd54ed644f6ecb1f9c184c283cbc4
gogenzollern <arseny.lomakin@gmail.com>
2024-01-18 20:37:00 +0300

    [testing] тесты на вывод примтивных типов

---
+++ compiler/tests/exam_tests/reading_and_printing/rust_all_primitive_types_printnig.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let ch = 'x';
+    println!("char is {}", ch);
+
+    let integ = 1000000000;
+    println!("int is {}", integ);
+    println!("int is {}", 100);
+
+    let f = 1.2;
+    println!("float is {}", f);
+    println!("float is {}", 1e10);
+
+    let bool_val = true;
+    let b1 = !bool_val;
+    println!("boolean is {}", bool_val);
+    println!("boolean is {}", b1);
+}
