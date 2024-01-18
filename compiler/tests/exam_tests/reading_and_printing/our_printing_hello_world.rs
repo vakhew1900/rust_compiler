3f226857a872240cfc46fe8beeee903a139cb062
gogenzollern <arseny.lomakin@gmail.com>
2024-01-18 17:20:06 +0300

    [testing] Hello World printing

---
+++ compiler/tests/exam_tests/reading_and_printing/our_printing_hello_world.rs
@@ -0,0 +1,13 @@
+/****
+
+                            Online Rust Compiler.
+                Code, Compile, Run and Debug Rust program online.
+Write your code in this editor and press "Run" button to execute it.
+
+***/
+
+
+fn main() {
+    let x =  "Hello World";
+    println("{}", x);
+}
---
+++ compiler/tests/exam_tests/reading_and_printing/rust_printing_hello_world.rs
@@ -0,0 +1,12 @@
+/******************************************************************************
+
+                            Online Rust Compiler.
+                Code, Compile, Run and Debug Rust program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+fn main() {
+    println!("Hello World");
+}
