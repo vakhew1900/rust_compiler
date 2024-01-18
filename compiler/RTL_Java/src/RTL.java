6a8b863f256bbad50fe6b99917f5b83984c4c7d3
gogenzollern <arseny.lomakin@gmail.com>
2024-01-18 23:51:04 +0300

    [RTL] Исправлен print_bool

--- compiler/RTL_Java/src/RTL.java
+++ compiler/RTL_Java/src/RTL.java
@@ -1,6 +1,6 @@
 public class RTL {
 
-    public static void println(String str, String val) {
+    public static void println_int(String str, String val) {
         if (!str.contains("{}")) {
             throw new IllegalArgumentException("Format string is not contains {}");
         }
@@ -28,7 +28,8 @@ public class RTL {
         System.out.println(str.replace("{}", String.valueOf(ch)));
     }
 
-    public static void println_bool(String str, boolean b) {
+    public static void println_bool(String str, int b_int) {
+        boolean b = b_int > 0;
         if (!str.contains("{}")) {
             throw new IllegalArgumentException("Format string is not contains {}");
         }
