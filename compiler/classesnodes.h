47d8ff4ea542ff03dcdaebd593e9638a73a36740
Чупинин Антон <chupinin@intervolga.ru>
2024-01-18 23:30:28 +0300

    [codegeneration] генерация кода for

--- compiler/classesnodes.h
+++ compiler/classesnodes.h
@@ -249,8 +249,10 @@ public:
 
     vector<char> generateInt(int value);
     vector<char> generateReturn(ExprNode* exprNode);
-    void  fillBreaks(vector<char> &body, vector<int> breakVec);
-    void  fillContinues(vector<char> &body, vector<int> continueVec);
+    void  fillBreaks(vector<char> &body, vector<int> breakVec, int shift = 0);
+    void  fillContinues(vector<char> &body, vector<int> continueVec, int shift = 0);
+    vector<char> generateFor();
+    vector<char> generateForEach();
 };
 
 class ExprListNode : public Node {
