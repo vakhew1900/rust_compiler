93559fe4e340d7bb9d1c58c1314aa79e2228f941
gogenzollern <arseny.lomakin@gmail.com>
2024-01-20 13:07:07 +0300

    [test] Объявления строк

---
+++ compiler/tests/exam_tests/var_declaration/string_decl.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // не работают
+//     let mut strg: String = r###"Это ##"raw"## строка с тройными кавычками"###;
+//     let raw = r"Это \0 \n \t \xFF строка с тройными кавычками";
+//     let multiline_string = "Это множественная
+// строка,
+// состоящая из нескольких строк."; //- ошибка вывода
+//     println("{}", multiline_string);
+    // Жесткая ошибка - конец на терминальном нуле
+//     let escaped_string = "Это строка с символами:\n \\n (новая строка),\t \\t (табуляция)\0 \\0 (терминальный ноль)";
+//     println("{}", escaped_string);
+//     println("{}", s);
+    //Все что ниже - работает
+    let quote_escape = "Это строка с символами \" внутри";
+    let ascii_escape = "ASCII символы: \x48\x65\x6C\x6C\x6F"; // Hello
+    let unicode_escape = "Символы Unicode: \u{0042} \u{0043} \u{0041}";
+    println("{}", quote_escape);
+    println("{}", ascii_escape);
+    println("{}", unicode_escape);
+}
