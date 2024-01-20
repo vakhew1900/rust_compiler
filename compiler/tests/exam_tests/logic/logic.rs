4d556d655cee24c093b5aa9708dc936b6f49f06b
gogenzollern <arseny.lomakin@gmail.com>
2024-01-20 13:49:52 +0300

    [test] тест на логику

---
+++ compiler/tests/exam_tests/logic/logic.rs
@@ -0,0 +1,12 @@
+fn main(){
+    // Логические операции
+    // Комбинирование
+    let a = true;
+    let b = false;
+    let c = true;
+
+    let result1 = a && b || c;
+    let result2 = !(a && b) || c;
+    println_bool("{}", result1);
+    println_bool("{}", result2);
+}
