24d9180c200152b4ed0a79e3e1c46c476e594b65
gogenzollern <arseny.lomakin@gmail.com>
2024-01-20 17:48:49 +0300

    [test] декларации с 0b, 0x, 0o  и прочие еканутики

--- compiler/tests/exam_tests/var_declaration/all_types_decl.rs
+++ compiler/tests/exam_tests/var_declaration/all_types_decl.rs
@@ -6,6 +6,14 @@ fn main() {
     let mut strs : String = "Petya";
     let x = 23___;
     println_int("{}", x);
-    //let mut s : &str = "vasya";
 
+    let decimal_integer = 4_2;          // Десятичное целое число (42, _ не несет смысловой нагрузки)
+    let hexadecimal_integer = 0x2A;    // Шестнадцатеричное целое число
+    let binary_integer = 0b101010;     // Двоичное целое число
+    let octal_integer = 0o52;          // Восьмеричное целое число
+
+    println_int("{}", decimal_integer);
+    println_int("{}", hexadecimal_integer);
+    println_int("{}", binary_integer);
+    println_int("{}", octal_integer);
 }
