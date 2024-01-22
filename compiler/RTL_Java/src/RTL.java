5f1690b805143b226dab7f7b555ee8982ec7df20
gogenzollern <arseny.lomakin@gmail.com>
2024-01-22 20:38:36 +0300

    [refactor] пересобрана RTL

--- compiler/RTL_Java/src/RTL.java
+++ compiler/RTL_Java/src/RTL.java
@@ -100,9 +100,29 @@ public class RTL {
      * Сравнение первой и второй строки
      * @param s1 - первая строка
      * @param s2 - вторая строка
-     * @return true если первая строка лексиграфически больше второй, false если равна или лексиграфически меньше второй
+     * @return true если первая строка лексикографически больше второй, false если равна или лексикографически меньше второй
      */
     public static boolean isBigger(String s1, String s2){
+        return s1.compareTo(s2) > 0;
+    }
+
+    /***
+     * Сравнение первой и второй строки
+     * @param s1 - первая строка
+     * @param s2 - вторая строка
+     * @return true если первая строка лексикографически больше или равна второй, false если лексикографически меньше второй
+     */
+    public static boolean isBiggerOrEqual(String s1, String s2){
+        return s1.compareTo(s2) >= 0;
+    }
+
+    /***
+     * Сравнение первой и второй строки
+     * @param s1 - первая строка
+     * @param s2 - вторая строка
+     * @return true если первая строка лексикографически меньше второй, false если равна или лексикографически больше второй
+     */
+    public static boolean isLower(String s1, String s2){
         return s1.compareTo(s2) < 0;
     }
 
@@ -110,10 +130,10 @@ public class RTL {
      * Сравнение первой и второй строки
      * @param s1 - первая строка
      * @param s2 - вторая строка
-     * @return true если первая строка лексиграфически меньше второй, false если равна или лексиграфически больше второй
+     * @return true если первая строка лексикографически меньше или равна второй, false если лексикографически больше второй
      */
-    public static boolean isLower(String s1, String s2){
-        return s1.compareTo(s2) > 0;
+    public static boolean isLowerOrEqual(String s1, String s2){
+        return s1.compareTo(s2) <= 0;
     }
 
     /***
@@ -125,5 +145,40 @@ public class RTL {
     public static boolean isEqual(String s1, String s2){
         return s1.compareTo(s2) == 0;
     }
+
+    /***
+     * Сравнение первой и второй строки
+     * @param s1 - первая строка
+     * @param s2 - вторая строка
+     * @return false если первая строка лексикографически равна со второй, true если лексикографически не равна второй
+     */
+    public static boolean isNotEqual(String s1, String s2){
+        return s1.compareTo(s2) != 0;
+    }
+
+    /***
+     * Склейка первой и второй строки
+     * @param s1 - первая строка
+     * @param s2 - вторая строка
+     * @return объединенные в одну строки
+     */
+    public static String push_str(String s1, String s2) {
+        StringBuilder stringBuilder = new StringBuilder(s1);
+        stringBuilder.append(s2);
+        return stringBuilder.toString();
+    }
+
+    /***
+     * Склейка строки с символом
+     * @param s - первая строка
+     * @param c- вторая строка
+     * @return объединенная с символом строка
+     */
+    public static String push_сhar(String s, char c) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        stringBuilder.append(c);
+        return stringBuilder.toString();
+    }
+
 }
 
