c96fbc431549a67b0ea4d9e27024c3b9e648edf7
gogenzollern <arseny.lomakin@gmail.com>
2024-01-23 22:15:56 +0300

    [test] тест на передачу объекта в функцию по значению и ссылке - правки

--- compiler/tests/complex_tests/rust_our/func_by_ref_and_value.rs
+++ compiler/tests/complex_tests/rust_our/func_by_ref_and_value.rs
@@ -2,13 +2,17 @@ struct MyStruct {
     data: i32,
 }
 
-fn by_value(obj: MyStruct) {
-    println_int("Function by_value: data = {}", obj.data);
+fn by_value(mut obj: MyStruct) {
+    println_int("Function by_value: data before = {}", obj.data);
+    obj.data = 999;
+    println_int("Function by_value: data modified = {}", obj.data);
     // obj будет уничтожен по завершении функции, так как передан по значению
 }
 
-fn by_reference(obj: &MyStruct) {
-    println_int("Function by_reference: data = {}", obj.data);
+fn by_reference(obj: &mut MyStruct) {
+    println_int("Function by_reference: data before = {}", obj.data);
+    obj.data = 888;
+    println_int("Function by_reference: data modified = {}", obj.data);
     // obj остается владельцем после завершения функции, так как передан по ссылке
 }
 
@@ -24,11 +28,15 @@ fn main() {
     //println_int("Main function: data = {}", my_object.data);
 
     // Создание нового объекта MyStruct
-    let my_object_ref = MyStruct { data: 123 };
+    let mut my_object_ref = MyStruct { data: 123 };
 
     // Вызов функции by_reference с передачей объекта по ссылке
-    by_reference(&my_object_ref);
+    by_reference(&mut my_object_ref);
 
     // my_object_ref все еще доступен после вызова функции by_reference
     println_int("Main function: data = {}", my_object_ref.data);
+
+    // ВАЖНО - в оригинальном расте и у нас, если передать объект в функцию
+    // по немутабельному значению, его нельзя изменять, но он исчезает (переходит во владение
+    // функции)
 }
