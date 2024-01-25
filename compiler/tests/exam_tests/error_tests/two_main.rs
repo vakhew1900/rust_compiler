96d8aef63f9a5ca219abe47e70d6ab59ecbf3d03
gogenzollern <arseny.lomakin@gmail.com>
2024-01-26 00:27:06 +0300

    [test] ошибочный тест - два мэйна

---
+++ compiler/tests/exam_tests/error_tests/two_main.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let x =  "string start";
+    let y = x.push_str(" string end");
+
+    println("{}", x);
+    println("{}", y);
+
+    println("{}", x.push_char('1'));
+}
+
+fn main(){
+
+}
